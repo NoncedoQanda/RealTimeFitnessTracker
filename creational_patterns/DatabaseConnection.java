2bd7bd0fcd7511fcaec20c90202450dd3bf8519e
NoncedoQanda <169073519+NoncedoQanda@users.noreply.github.com>
2025-04-20 18:50:13 +0200

    Create DatabaseConnection.java

---
+++ creational_patterns/DatabaseConnection.java
@@ -0,0 +1,16 @@
+package com.fitness.tracker.patterns;
+
+public class DatabaseConnection {
+    private static DatabaseConnection instance;
+
+    private DatabaseConnection() {}
+
+    public static synchronized DatabaseConnection getInstance() {
+        if (instance == null) {
+            instance = new DatabaseConnection();
+        }
+        return instance;
+    }
+
+    public void connect() { /* Connect to DB */ }
+}
