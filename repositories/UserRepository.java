4e637e7502116077732ac21f5e3bbc19192ab393
NoncedoQanda <169073519+NoncedoQanda@users.noreply.github.com>
2025-04-27 11:56:06 +0200

    Create UserRepository.java

---
+++ repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.fitness.tracker.repositories;
+
+import com.fitness.tracker.User;
+
+public interface UserRepository extends Repository<User, String> {
+    // Add custom methods if needed (e.g., findByEmail)
+}
