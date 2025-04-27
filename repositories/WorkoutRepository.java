7f042ab1f46abc0a65017611ddb136cf6f32ef13
NoncedoQanda <169073519+NoncedoQanda@users.noreply.github.com>
2025-04-27 11:59:39 +0200

    Create WorkoutRepository.java

---
+++ repositories/WorkoutRepository.java
@@ -0,0 +1,7 @@
+package com.fitness.tracker.repositories;
+
+import com.fitness.tracker.Workout;
+
+public interface WorkoutRepository extends Repository<Workout, String> {
+    // Add custom methods if needed
+}
